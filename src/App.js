a322ac7a66a3fa8904c2deb96964d330ba056cc0
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-15 21:58:40 +0100

    added logic to load Tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import TaskList from "./components/TaskList/TaskList";
+import { loadTasksThunk } from "./redux/thunks/thunks";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTasksThunk);
+  }, [dispatch]);
+
   return <TaskList />;
 }
 
