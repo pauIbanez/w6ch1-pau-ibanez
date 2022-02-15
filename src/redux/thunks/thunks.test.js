72046dd45fe39b999282b7197ed9d1729c05da11
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-15 21:39:20 +0100

    added propper thunk tests

--- src/redux/thunks/thunks.test.js
+++ src/redux/thunks/thunks.test.js
@@ -7,7 +7,7 @@ import {
 } from "./thunks";
 
 describe("Given loadTasksThunk", () => {
-  describe("When it is called", () => {
+  describe("When it is called and API is ok", () => {
     test("Then it should call dispatch passing a loadTasksAction", async () => {
       const tasks = [
         {
@@ -36,10 +36,32 @@ describe("Given loadTasksThunk", () => {
       expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
     });
   });
+
+  describe("When it is called and API is caca", () => {
+    const OLD_ENV = process.env;
+
+    beforeEach(() => {
+      jest.resetModules(); // Most important - it clears the cache
+      process.env = { ...OLD_ENV }; // Make a copy
+    });
+    afterAll(() => {
+      process.env = OLD_ENV; // Restore old environment
+    });
+    test("Then it should not call dispatch", async () => {
+      jest.resetModules();
+
+      process.env.REACT_APP_API_URL = "https://failtherequest.com/thisworks";
+      const mockDispatch = jest.fn();
+
+      await loadTasksThunk(mockDispatch);
+
+      expect(mockDispatch).not.toHaveBeenCalled();
+    });
+  });
 });
 
 describe("Given addTaskThunk", () => {
-  describe("When it is called", () => {
+  describe("When it is called and api ok", () => {
     test("Then it should call dispatch passing an addTask action", async () => {
       const task = {
         name: "Task 3",
@@ -66,10 +88,23 @@ describe("Given addTaskThunk", () => {
       expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
     });
   });
+
+  describe("When it is called and api caca", () => {
+    test("Then it should not call dispatch", async () => {
+      const task = ["fail"];
+
+      const mockDispatch = jest.fn();
+
+      const addTaskThunk = getAddTaskThunk(task);
+      await addTaskThunk(mockDispatch);
+
+      expect(mockDispatch).not.toHaveBeenCalled();
+    });
+  });
 });
 
 describe("Given removeTaskThunk", () => {
-  describe("When it is called", () => {
+  describe("When it is called with and id 3", () => {
     test("Then it should call dispatch passing a removeTask action", async () => {
       const id = 3;
 
@@ -86,10 +121,22 @@ describe("Given removeTaskThunk", () => {
       expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
     });
   });
+
+  describe("When it is called with and id 2", () => {
+    test("Then it should not call dispatch", async () => {
+      const id = 2;
+      const mockDispatch = jest.fn();
+
+      const deleteTaskThunk = getRemoveTaskThunk(id);
+      await deleteTaskThunk(mockDispatch);
+
+      expect(mockDispatch).not.toHaveBeenCalled();
+    });
+  });
 });
 
 describe("Given toggleTaskThunk", () => {
-  describe("When it is called", () => {
+  describe("When it is called with id 3", () => {
     test("Then it should call dispatch passing a removeTask action", async () => {
       const id = 3;
       const newCompleated = false;
@@ -105,6 +152,19 @@ describe("Given toggleTaskThunk", () => {
       await toggleTaskThunk(mockDispatch);
 
       expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+
+  describe("When it is called with id 2", () => {
+    test("Then it should not call dispatch", async () => {
+      const id = 2;
+      const newCompleated = false;
+      const mockDispatch = jest.fn();
+
+      const toggleTaskThunk = getToggleTaskThunk(newCompleated, id);
+      await toggleTaskThunk(mockDispatch);
+
+      expect(mockDispatch).not.toHaveBeenCalled();
     });
   });
 });
