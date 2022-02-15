48d66c95229e05261372bdad3559f668d0038e7e
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-16 00:34:01 +0100

    added some stats

--- src/components/InfoContainer/InfoContainer.js
+++ src/components/InfoContainer/InfoContainer.js
@@ -4,10 +4,46 @@ const Section = styled.section`
   width: 300px;
   position: relative;
   border: 1px solid #5a5a5a;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 100px;
+  padding-left: 30px;
+`;
+
+const InfoItem = styled.p`
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  gap: 7px;
+`;
+
+const ItemData = styled.span`
+  font-size: 30px;
 `;
 
 const InfoContainer = () => {
-  return <Section />;
+  return (
+    <Section>
+      <InfoItem>
+        Total Tasks:
+        <ItemData>0</ItemData>
+      </InfoItem>
+
+      <InfoItem>
+        Compleated Tasks:
+        <ItemData>0</ItemData>
+      </InfoItem>
+
+      <InfoItem>
+        Ayabababadu:
+        <ItemData>0</ItemData>
+      </InfoItem>
+    </Section>
+  );
 };
 
 export default InfoContainer;
