6cfb6262eb8d1d953efce26095768e34fd045986
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-15 23:49:46 +0100

    added more usless styled

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -2,16 +2,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 
+const padding = 50;
+
 const ListItem = styled.li`
   background-color: #fffbf5;
   height: 50px;
-  border-bottom: 1px solid gray;
+
+  ${(props) => (props.compleated ? "color: #202020;" : "color: #5a5a5a;")}
+
+  border-bottom: 1px solid #5a5a5a;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-left: 50px;
+  padding-left: ${padding}px;
+  user-select: none;
+  cursor: pointer;
 
   &:hover {
+    color: #202020;
+    background-color: #f5f1eb;
     button {
       visibility: visible;
     }
@@ -23,44 +32,42 @@ const TaskName = styled.h2`
   font-weight: 100;
 `;
 
-const TaskControlls = styled.div`
+const DeleteButton = styled.button`
+  width: ${padding}px;
   display: flex;
-  gap: 20px;
-  margin-right: 30px;
-`;
-
-const ButtonIcon = styled.button`
+  justify-content: center;
   color: #93b2e6;
-  ${(props) => (props.compleated ? "color: #BDE6AD;" : "visibility: hidden;")}
+  visibility: hidden;
   background-color: inherit;
   border: none;
   cursor: pointer;
-`;
+  font-size: 30px;
 
-const DeleteButton = styled(ButtonIcon)`
   &:hover {
     color: #ff6666;
   }
 `;
 
-const CompleteButton = styled(ButtonIcon)`
-  &:hover {
-    color: #bde6ad;
-  }
+const CompleteIcon = styled.div`
+  position: absolute;
+  left: 0;
+  width: ${padding}px;
+  display: flex;
+  justify-content: center;
+  ${(props) => (props.compleated ? "color: #BDE6AD;" : "display: none;")}
+  font-size: 30px;
 `;
 
 const Task = ({ task }) => {
   return (
-    <ListItem>
+    <ListItem compleated={task.compleated}>
+      <CompleteIcon compleated={task.compleated}>
+        <FontAwesomeIcon icon={faCircleCheck} />
+      </CompleteIcon>
       <TaskName>{task.text}</TaskName>
-      <TaskControlls>
-        <DeleteButton>
-          <FontAwesomeIcon icon={faTrashCan} size="2x" />
-        </DeleteButton>
-        <CompleteButton compleated={task.compleated}>
-          <FontAwesomeIcon icon={faCircleCheck} size="2x" />
-        </CompleteButton>
-      </TaskControlls>
+      <DeleteButton>
+        <FontAwesomeIcon icon={faTrashCan} />
+      </DeleteButton>
     </ListItem>
   );
 };
