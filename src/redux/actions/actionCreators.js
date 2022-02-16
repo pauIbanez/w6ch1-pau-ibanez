1945c3af0a1e4f56e6c50d8b1fe7fbb8d67013b3
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-15 19:03:26 -0800

    added rest of tasks

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -4,3 +4,18 @@ export const loadTasksAction = (tasks) => ({
   type: actionTypes.loadTasks,
   tasks,
 });
+
+export const addTaskAction = (task) => ({
+  type: actionTypes.addTask,
+  task,
+});
+
+export const removeTaskAction = (id) => ({
+  type: actionTypes.removeTask,
+  id,
+});
+
+export const toggleTaskAction = (id) => ({
+  type: actionTypes.toggleTask,
+  id,
+});
