31107f37d48df8a90729bd2d7e48093e83f3799f
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-15 19:07:10 -0800

    added tests for the rest of actions

--- src/redux/actions/actionCreators.test.js
+++ src/redux/actions/actionCreators.test.js
@@ -1,9 +1,14 @@
-import { loadTasksAction } from "./actionCreators";
+import {
+  addTaskAction,
+  loadTasksAction,
+  removeTaskAction,
+  toggleTaskAction,
+} from "./actionCreators";
 import actionTypes from "./actionTypes";
 
 describe("Given loadTasksAction", () => {
   describe("When it is called passing an array of tasks", () => {
-    test("Then it should retur an action with type loadTasks", () => {
+    test("Then it should return an action with type loadTasks and the tasks", () => {
       const tasks = [
         {
           id: 1,
@@ -25,6 +30,62 @@ describe("Given loadTasksAction", () => {
       };
 
       const action = loadTasksAction(tasks);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given addTaskAction", () => {
+  describe("When it is called passing a task", () => {
+    test("Then it should return an action with type addTask and the task", () => {
+      const task = {
+        id: 1,
+        name: "Task 1",
+        dueDate: Date.now(),
+        compleated: false,
+      };
+
+      const expectedAction = {
+        type: actionTypes.addTask,
+        task,
+      };
+
+      const action = addTaskAction(task);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given removeTaskAction", () => {
+  describe("When it is called passing an id", () => {
+    test("Then it should return an action with type removeTask and the id", () => {
+      const id = 2;
+
+      const expectedAction = {
+        type: actionTypes.removeTask,
+        id,
+      };
+
+      const action = removeTaskAction(id);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given toggleTaskAction", () => {
+  describe("When it is called passing an id", () => {
+    test("Then it should return an action with type toggleTask and the id", () => {
+      const id = 3;
+
+      const expectedAction = {
+        type: actionTypes.toggleTask,
+        id,
+      };
+
+      const action = toggleTaskAction(id);
 
       expect(action).toEqual(expectedAction);
     });
