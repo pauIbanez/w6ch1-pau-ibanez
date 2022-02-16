2d4b7fb28c9f5701c153cd1da9d74a915bdd2228
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-15 20:04:12 -0800

    added test for loadTasksThunk and installedmsw

---
+++ src/redux/thunks/thunks.test.js
@@ -0,0 +1,34 @@
+import actionTypes from "../actions/actionTypes";
+import { loadTasksThunk } from "./thunks";
+
+describe("Given loadTasksThunk", () => {
+  describe("When it is called", () => {
+    test("Then it should call dispatch passing a loadTasksAction", () => {
+      const tasks = [
+        {
+          id: 1,
+          name: "Task 1",
+          dueDate: "Date.now()",
+          compleated: false,
+        },
+        {
+          id: 2,
+          name: "Task 2",
+          dueDate: "Date.now()",
+          compleated: true,
+        },
+      ];
+
+      const expectedAction = {
+        type: actionTypes.loadTasks,
+        tasks,
+      };
+
+      const mockDispatch = jest.fn();
+
+      loadTasksThunk(mockDispatch);
+
+      expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+});
